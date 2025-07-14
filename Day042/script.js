1f40a535aaef00ed1fdbf2fd5b4222ce44986b28
Alaa' Eliwa <115268024+alaaeliwa@users.noreply.github.com>
2025-07-14 12:46:02 +0300

    Add files via upload

---
+++ Day042/script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", this.window.scrollY > 60);
+});
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
+};
